7320675beef9cb851a6fab12957a1ed033bd3e42
Kristen Norris <krnorris65@gmail.com>
2017-10-13 10:38:26 -0500

    made modifications to blog.html

--- blog-controller.js
+++ blog-controller.js
@@ -7,28 +7,30 @@ for (let i = 0; i < storedBlogPosts.length; i++) {
     let currentBlog = storedBlogPosts[i];
     
     blogElement.innerHTML += `
+    <!-- Beginning of Blog Post ${currentBlog.week_num} -->
     <article id="week-${currentBlog.week_num}" class="blog">
-        <header>
-            <h2 class="weekNum">${currentBlog.title}</h2>
-            <p class="weekDate">${currentBlog.week_dates}</p>
-        </header>
-
-        <section>
-            <h3>Celebrations & Inspirations</h3>
-            <li>${currentBlog.content.celebrations.celebration_1}</li>
-            <li>${currentBlog.content.celebrations.celebration_2}</li>
-            <li>${currentBlog.content.celebrations.celebration_3}</li>
-            </section>
-            
-            <section>
-            <h3>Challenges & Hang-Ups</h3>
-            <li>${currentBlog.content.challenges.challenge_1}</li>
-            <li>${currentBlog.content.challenges.challenge_2}</li>
-            <li>${currentBlog.content.challenges.challenge_3}</li>
-        </section>
-        
-        <footer>
-        <span>Posted by ${currentBlog.author} on ${currentBlog.date_of_publication}</time></span>
-        </footer>
+    <header>
+    <h2 class="weekNum">${currentBlog.title}</h2>
+    <p class="weekDate">${currentBlog.week_dates}</p>
+    </header>
+    
+    <section>
+    <h3>Celebrations & Inspirations</h3>
+    <li>${currentBlog.content.celebrations.celebration_1}</li>
+    <li>${currentBlog.content.celebrations.celebration_2}</li>
+    <li>${currentBlog.content.celebrations.celebration_3}</li>
+    </section>
+    
+    <section>
+    <h3>Challenges & Hang-Ups</h3>
+    <li>${currentBlog.content.challenges.challenge_1}</li>
+    <li>${currentBlog.content.challenges.challenge_2}</li>
+    <li>${currentBlog.content.challenges.challenge_3}</li>
+    </section>
+    
+    <footer>
+    <span>Posted by ${currentBlog.author} on ${currentBlog.date_of_publication}</time></span>
+    </footer>
+    <!-- End of Blog Post ${currentBlog.week_num} -->
     `
 }
