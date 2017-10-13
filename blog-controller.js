ade90cb8eb711923b3c650a630027b49f10c098b
Kristen Norris <krnorris65@gmail.com>
2017-10-13 10:27:28 -0500

    created code to insert blog objects into blog.html

---
+++ blog-controller.js
@@ -0,0 +1,34 @@
+//parse to read
+let storedBlogPosts = JSON.parse(localStorage.getItem("blogPosts"));
+let blogElement = document.getElementById("blog-posts");
+
+//loop through blog array and insert blog posts into html
+for (let i = 0; i < storedBlogPosts.length; i++) {
+    let currentBlog = storedBlogPosts[i];
+    
+    blogElement.innerHTML += `
+    <article id="week-${currentBlog.week_num}" class="blog">
+        <header>
+            <h2 class="weekNum">${currentBlog.title}</h2>
+            <p class="weekDate">${currentBlog.week_dates}</p>
+        </header>
+
+        <section>
+            <h3>Celebrations & Inspirations</h3>
+            <li>${currentBlog.content.celebrations.celebration_1}</li>
+            <li>${currentBlog.content.celebrations.celebration_2}</li>
+            <li>${currentBlog.content.celebrations.celebration_3}</li>
+            </section>
+            
+            <section>
+            <h3>Challenges & Hang-Ups</h3>
+            <li>${currentBlog.content.challenges.challenge_1}</li>
+            <li>${currentBlog.content.challenges.challenge_2}</li>
+            <li>${currentBlog.content.challenges.challenge_3}</li>
+        </section>
+        
+        <footer>
+        <span>Posted by ${currentBlog.author} on ${currentBlog.date_of_publication}</time></span>
+        </footer>
+    `
+}
--- blog-factory.js
+++ blog-factory.js
@@ -1,6 +1,8 @@
 const week1 = {
     "title": "Week 1", 
-    "date of publication": "2017-10-06", 
+    "date_of_publication": "10-06-2017",
+    "week_num": "1",
+    "week_dates": "October 2 - 6, 2017", 
     "tags": "celebrations, inspirations, challenges, hang-ups", 
     "author": "Kristen",
     "content": {
@@ -10,9 +12,9 @@ const week1 = {
             "celebration_3": "Was able to figure out the Overly Excited JavaScript exercise without feeling completely overwhelmed"
         },
         "challenges": {
-            "challenges_1": "Remembering all of the commands for the terminal",
-            "challenges_2": "Figuring out the best way to organize code",
-            "challenges_3": "Not sure where to start with the Cash-to-Coin JavaScript exercise"
+            "challenge_1": "Remembering all of the commands for the terminal",
+            "challenge_2": "Figuring out the best way to organize code",
+            "challenge_3": "Not sure where to start with the Cash-to-Coin JavaScript exercise"
         }
     }
 }
@@ -26,7 +28,3 @@ blogPosts.push(week1);
 let blogPostsString = JSON.stringify(blogPosts);
 localStorage.setItem("blogPosts", blogPostsString);
 
-//parse to read
-let storedBlogPosts = JSON.parse(localStorage.getItem("blogPosts"));
-
-console.log(storedBlogPosts); 
