2e1025b2ada1e1db0b063206ab729615ea0c5b6a
Kristen Norris <krnorris65@gmail.com>
2017-10-18 14:08:59 -0500

    formatting to dynamic nav bar

--- navBar/nav-controller.js
+++ navBar/nav-controller.js
@@ -5,8 +5,9 @@ const navElement = document.getElementById("nav-items");
 //loop through nav array and write each nav element to the Nav Bar
 for (let i = 0; i < navBarDatabase.length; i++) {
     let currentNav = navBarDatabase[i];
-
+    
     navElement.innerHTML += `
     <li><a href="${currentNav.link}">${currentNav.page}</a></li>
     `
 }
+
