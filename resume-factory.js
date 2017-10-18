2e0a40ef444d798a85e39f56f87601f68131d6ab
Kristen Norris <krnorris65@gmail.com>
2017-10-18 11:26:50 -0500

    summarized job history responsibilities to make it easier to put in database

--- resume-factory.js
+++ resume-factory.js
@@ -5,7 +5,8 @@ const devSpecialist = {
     "location": "Nashville, TN",
     "startDate": "January 2016",
     "endDate": "October 2017",
-    "responsibilities": ""
+    "summary": "Offered general support to the development team.",
+    "responsibilities": "Responsibilities included: tracking donations and making sure donors were properly acknowledged, assisting with marketing efforts, creating a dashboard to report fundraising and outreach progress each month, and assisted with the annual fall fundraiser."
 }
 
 //Job 2
@@ -15,7 +16,8 @@ const vetCaseManager = {
     "location": "Nashville, TN",
     "startDate": "May 2015",
     "endDate": "November 2015",
-    "responsibilities": ""
+    "summary": "Provided case management to homeless veterans in the Veterans Per Diem program.",
+    "responsibilities": "Responsibilities included: assisting with daily functions, tracking and reporting demographic information for grant purposes, creating and distributing a bi-weekly chore chart, and managing TN-WITS billing for the Guest House Recovery Program."
 }
 
 //Job 3
@@ -25,7 +27,8 @@ const caseManager = {
     "location": "Nashville, TN",
     "startDate": "August 2013",
     "endDate": "May 2015",
-    "responsibilities": ""
+    "summary": "Provided case management to homeless individuals and families.",
+    "responsibilities": "Responsibilities included: managing a caseload of over 300 homeless individuals, helping individuals acquire the necessary documents to obtain housing, maintaining our database to ensure information is properly tracked, and supporting the day-to-day operations of the activity center."
 }
 
 //Job 4
@@ -35,7 +38,8 @@ const jvOdyssey = {
     "location": "Nashville, TN",
     "startDate": "August 2012",
     "endDate": "August 2013",
-    "responsibilities": ""
+    "summary": "Helped with running a post-treatment recovery program for chronically homeless men.",
+    "responsibilities": "Responsibilities included: managing volunteers, conducting daily community meetings, planning recreational activities, and leading a weekly mediation class."
 }
 
 //Job 5
@@ -45,7 +49,8 @@ const progamSupervisor = {
     "location": "Pine Ridge, SD",
     "startDate": "June 2012",
     "endDate": "July 2012",
-    "responsibilities": ""
+    "summary": "Carried out various tasks to ensure the weekly volunteer program ran smoothly.",
+    "responsibilities": "Responsibilities included: assisting the Construction Coordinators, preparing lunches, cleaning the facility and answering questions from volunteers."
 }
 
 //Array of jobs
