6f4239484807e5af0433d8fb4b9826cc0e981a80
Kristen Norris <krnorris65@gmail.com>
2017-10-19 15:24:44 -0500

    added pagination to blog page

--- blog/blog-controller.js
+++ blog/blog-controller.js
@@ -9,16 +9,16 @@ for (let i = 0; i < storedBlogPosts.length; i++) {
     let currentBlog = storedBlogPosts[i];
     
     finalBlogString += `
-    <!-- Beginning of Blog Post ${currentBlog.week_num} -->
-    <article id="week-${currentBlog.week_num}" class="blog">
-    <header>
-    <h2 class="weekNum">${currentBlog.title}</h2>
-    <p class="weekDate">${currentBlog.week_dates}</p>
-    </header>
-    
-    <section>
-    <h3>Celebrations & Inspirations</h3>
-    <ul>`
+        <!-- Beginning of Blog Post ${currentBlog.week_num} -->
+        <article id="week-${currentBlog.week_num}" class="blog">
+        <header>
+        <h2 class="weekNum">${currentBlog.title}</h2>
+        <p class="weekDate">${currentBlog.week_dates}</p>
+        </header>
+        
+        <section>
+        <h3>Celebrations & Inspirations</h3>
+        <ul>`
 
     //iterates over celebration array
     for (let j = 0; j < currentBlog.content.celebrations.length; j++) {
@@ -26,12 +26,12 @@ for (let i = 0; i < storedBlogPosts.length; i++) {
     }
     
     finalBlogString += `
-    </ul>
-    </section>
-    
-    <section>
-    <h3>Challenges & Hang-Ups</h3>
-    <ul>`
+        </ul>
+        </section>
+        
+        <section>
+        <h3>Challenges & Hang-Ups</h3>
+        <ul>`
     
     //iterates over challenges array
     for (let s = 0; s < currentBlog.content.challenges.length; s++) {
@@ -39,14 +39,14 @@ for (let i = 0; i < storedBlogPosts.length; i++) {
     };
    
     finalBlogString += `
-    </ul>
-    </section>
-    
-    <footer>
-    <span>Posted by ${currentBlog.author} on ${currentBlog.date_of_publication}</time></span>
-    </footer>
-    <!-- End of Blog Post ${currentBlog.week_num} -->
-    `
+        </ul>
+        </section>
+        
+        <footer>
+        <span>Posted by ${currentBlog.author} on ${currentBlog.date_of_publication}</time></span>
+        </footer>
+        <!-- End of Blog Post ${currentBlog.week_num} -->
+        `
     //update DOM
     blogElement.innerHTML += finalBlogString;
 } 
