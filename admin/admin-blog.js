630921499d2e111e3c0399e272fc44adbd632213
Kristen Norris <krnorris65@gmail.com>
2017-10-24 15:54:05 -0500

    added factory function to add more celebration/challenge input to the DOM

--- admin/admin-blog.js
+++ admin/admin-blog.js
@@ -1,10 +1,33 @@
 const celebrationButton = document.getElementById("add-celebration")
-const celebrationEl = document.getElementById("form_celebrations")
+const challengeButton = document.getElementById("add-challenge")
 
 
-addCelebration = function(){
+//generator function to create a unique id for the celebration/challenge created by user. starts at 2 since the first input is already on the page
+const idGen = function* () {
+    let id = 2
+    
+    while (true) {
+        yield id
+        id += 1
+    }
+}
+const celebrationIdFactory = idGen()
+const challengeIdFactory = idGen()
+
+const addCelebration = function(){
+    const celebrationId = celebrationIdFactory.next().value
+    const celebrationEl = document.getElementById("form_celebrations")
     celebrationEl.innerHTML += `
-    <input type="text" name="celebration1">`
+    <input type="text" name="celebration" id="form_celebration_${celebrationId}">
+    `
+}
+const addChallenge = function(){
+    const challengeId = challengeIdFactory.next().value
+    const challengeEl = document.getElementById("form_challenges")
+    challengeEl.innerHTML += `
+        <input type="text" name="challenge" id="form_challenge_${challengeId}">
+    `
 }
 
 celebrationButton.addEventListener("click", addCelebration)
+challengeButton.addEventListener("click", addChallenge)
