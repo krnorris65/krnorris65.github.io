ba79f0c6f18691562f8c1734b021f3fcdc3eb80e
Kristen Norris <krnorris65@gmail.com>
2017-11-15 15:45:06 -0600

    commented code

--- admin/scripts/blogEditList.js
+++ admin/scripts/blogEditList.js
@@ -5,35 +5,32 @@ const blogDatabase = JSON.parse(localStorage.getItem("adminDatabase")).blog; //g
 //if there are blogs in local storage, then display their title in the Edit Blog section with an edit button (sort by id). 
 
 if(blogDatabase.length > 0) {
+    // adds header text to edit blog section
     const eHeaderEl = document.createElement("h1")
     const eHeaderTxt = document.createTextNode("Edit an Existing Blog")
-
     eHeaderEl.appendChild(eHeaderTxt)
     editEl.appendChild(eHeaderEl)
-    editEl.classList.add("hideContent")
+    editEl.classList.add("hideContent") //remove class that hides element
 
-    let blogString = ""
-
-    const displayBlogList = blogDatabase.sort(
-        (a, b) => {
-            return b.id - a.id
-        }
-    ).forEach( blog => {
+    //creates an unordered list for the list of blogs
+    const blogListEl = document.createElement("ul")
+    editEl.appendChild(blogListEl)
+    
+    //iterates through existing blogs and adds them to the the blog list with an edit button
+    const displayBlogList = blogDatabase
+        .sort((a, b) => b.id - a.id) //have most recent blog on top
+        .forEach( blog => {
         
-        
-        const blogList = document.createElement("li")
-        blogList.appendChild(document.createTextNode(`${blog.title} `))
+        const blogItem = document.createElement("li")
+        blogItem.appendChild(document.createTextNode(`${blog.title} `))
 
         const editButton = document.createElement("button")
         editButton.appendChild(document.createTextNode("Edit"))
         editButton.id = "edit_" + `${blog.id}`
 
-        blogList.appendChild(editButton)
-        editEl.appendChild(blogList)
-
-
+        blogItem.appendChild(editButton)
+        blogListEl.appendChild(blogItem)
 
     })
 
-    editEl.innerHTML += blogString
 }
