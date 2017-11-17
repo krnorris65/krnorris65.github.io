dc326fc1ee5f9fd8aece2627ba60f7ede7d92f30
Kristen Norris <krnorris65@gmail.com>
2017-11-17 14:22:55 -0600

    changed grunt file to include browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,68 +6,53 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             scripts: {
-            files: ['**/*.js'],
-            tasks: ['uglify'],
+            files: ['**/scripts/*.js', "!node_modules/**/*.js"],
+            tasks: ["eslint", "browserify", "uglify"],
             options: {
                 spawn: false,
             },
             }
         },
+        browserify: {
+            options: {
+                browserifyOptions: {
+                    debug: true
+                }
+            },
+            dist: {
+                files: {
+                    "build/bundle.js": ["scripts/main.js"],
+                    
+                },
+            },
+        },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
             },
             build: {
                 files: [{
                     expand: true,
-                    cwd: 'admin/scripts',
-                    src: '*.js',
-                    dest: 'admin/build',
-                    ext: '.min.js'
-                },{
-                expand: true,
-                cwd: 'blog/scripts',
-                src: '*.js',
-                dest: 'blog/build',
-                ext: '.min.js'
-            },{
-                expand: true,
-                cwd: 'contact/scripts',
-                src: '*.js',
-                dest: 'contact/build',
-                ext: '.min.js'
-            },{
-                expand: true,
-                cwd: 'projects/scripts',
-                src: '*.js',
-                dest: 'projects/build',
-                ext: '.min.js'
-            },{
-                expand: true,
-                cwd: 'resume/scripts',
-                src: '*.js',
-                dest: 'resume/build',
-                ext: '.min.js'
-            },{
-                expand: true,
-                cwd: 'scripts',
-                src: '*.js',
-                dest: 'build',
-                ext: '.min.js'
-            }]
+                    cwd: "build",
+                    src: "bundle.js",
+                    dest: "build",
+                    ext: ".min.js"
+                }]
             }
         },
         eslint: {
-            target: ['scripts/*.js']
+            all: ["**/*.js", "!node_modules/**/*.js", "!build/*.js"]
         }
     });
 
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-browserify")
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-eslint");
+    
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'eslint', 'watch']);
+    grunt.registerTask("default", ["watch", "browserify", "uglify", "eslint"]);
   
   };
